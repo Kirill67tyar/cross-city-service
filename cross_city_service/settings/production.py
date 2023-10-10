20c27d5a3dd55b5d0a0d914c8320ea22af140998
Kirill Bogomolov <kirillbogomolov.ric@gmail.com>
2023-10-10 21:42:14 +0500

    add fixture - mydata.json

--- cross_city_service/settings/production.py
+++ cross_city_service/settings/production.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
+    # 'debug_toolbar',
     'rest_framework',
     'orders.apps.OrdersConfig',
     'drivers.apps.DriversConfig',
@@ -61,7 +61,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'cross_city_service.urls'
@@ -159,7 +159,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # -------------------------------------------------------- for django debug toolbar
 # https://django-debug-toolbar.readthedocs.io/en/latest/index.html
 
-INTERNAL_IPS = ('127.0.0.1',)
+# INTERNAL_IPS = ('127.0.0.1',)
 
 """
 # -----------------------------------------------------------DB postgres
