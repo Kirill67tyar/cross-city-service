b13683d853bac599a43d9cb554d44fe6237e2272
Kirill Bogomolov <kirillbogomolov.ric@gmail.com>
2023-10-31 17:48:18 +0500

    change cross_city_service/urls.py

--- cross_city_service/urls.py
+++ cross_city_service/urls.py
@@ -26,11 +26,11 @@ urlpatterns = [
     # path('core/', include('orders.urls', namespace='orders')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        prefix=settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+# if settings.DEBUG:
+#     urlpatterns += static(
+#         prefix=settings.MEDIA_URL,
+#         document_root=settings.MEDIA_ROOT
+#     )
 # for django debug toolbar
 # urlpatterns.append(path('__debug__/', include('debug_toolbar.urls')))
 
